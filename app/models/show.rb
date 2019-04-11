492be16b53f12e6878c06297eeefeb35ce464fa0
Kelsey Low <kklowlow@gmail.com>
2019-04-11 02:40:40 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+    
+    def self.highest_rating
+      self.
+    end
+
+    def self.most_popular_show
+      self.
+    end
+
+    def self.lowest_rating
+      self.
+    end
+
+    def self.least_popular_show
+      self.
+    end
+
+    def self.ratings_sum
+      self.
+    end
+
+    def self.popular_shows
+      self.
+    end
+
+    def self.shows_by_alphabetical_order
+      self.
+    end
+    
+end
