5410363b091d34473213a08265da1afe340a3676
Kelsey Low <kklowlow@gmail.com>
2019-04-18 23:30:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,5 @@
+
+=begin
 class Show < ActiveRecord::Base
     
     def self.highest_rating
@@ -35,4 +37,38 @@ class Show < ActiveRecord::Base
       self.order(:name)
     end
     
+end
+=end 
+
+require 'pry'
+
+class Show < ActiveRecord::Base
+    
+    def self.highest_rating
+        self.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        self.where("rating = ?", self.highest_rating).first
+    end
+
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        self.where("rating = ?", self.lowest_rating).first
+    end
+
+    def self.ratings_sum
+        self.sum(:rating)
+    end
+
+    def self.popular_shows
+        self.where("rating > ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+        self.order(:name)
+    end
 end
