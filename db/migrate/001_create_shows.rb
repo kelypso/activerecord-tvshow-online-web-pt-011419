785d8e1e79be0dc351422e5fab9e711092fb0220
Kelsey Low <kklowlow@gmail.com>
2019-04-18 23:32:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
-class CreateShows < ActiveRecord::Migration
-    def change
-        create_table :shows do |column|
-            column.string :name
-            column.string :network
-            column.string :day
-            column.integer :rating
-        end
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
+  end
 end
