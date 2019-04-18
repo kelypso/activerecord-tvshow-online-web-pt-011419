6bf68148b545fb5b50830d1e66da3fa4bffe9817
Kelsey Low <kklowlow@gmail.com>
2019-04-18 23:30:15 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,6 +1,13 @@
+=begin
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :season, :string
   end
 end
+=end
 
+class AddSeasonToShows < ActiveRecord::Migration
+    def change
+        add_column :shows, :season, :string
+    end
+end
